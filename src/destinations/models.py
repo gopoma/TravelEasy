79942253d6135e3103166473459d24b26d564741
Gustavo <gordono@unsa.edu.pe>
2022-06-09 23:45:59 -0500

    Se ha desarrollado el model de Destination, además que se agregó la app de destinations en las aplicaciones que maneja Django, se crearon las migraciones, se instaló el paquete Pillow para las imágenes y se migró exitosamente

--- src/destinations/models.py
+++ src/destinations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    name = models.CharField(max_length=255)
+    img = models.ImageField(upload_to="pics")
+    description = models.TextField()
+    price = models.FloatField()
+    is_on_offer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} [Is on offer?: {self.is_on_offer}]"
