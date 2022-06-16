80f22dd85d5fc88384b37eea5339036672559335
Gustavo <gordono@unsa.edu.pe>
2022-06-15 23:04:13 -0500

    En primera instancia, se ha agregado el directorio media en el .gitignore porque es autogenerable por la configuración en settings.py, después se vuelve a construir el modelo como DestinosTuristicos para que concuerde con la hoja de Laboratorio, en el proceso también se lel declara su toString y se borran las migraciones anteriores, el db.sqlite3 para comenzar de nuevo con una tabla consistente y no tener data muerta, aunque tendré que crear de nuevo el superusuario

--- src/destinations/models.py
+++ src/destinations/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Destination(models.Model):
-    name = models.CharField(max_length=255)
-    img = models.ImageField(upload_to="pics")
-    description = models.TextField()
-    price = models.FloatField()
-    is_on_offer = models.BooleanField(default=False)
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.charField(max_length=255)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to="pics")
+    precioTour = models.FloatField()
+    ofertaTour = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.name} [Is on offer?: {self.is_on_offer}]"
+        return f"{self.nombreCiudad} [Is on offer?: {self.ofertaTour}]"
