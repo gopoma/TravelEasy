003a157815e9fa3508bc92246d9c8c0e077db7f7
Gustavo <gordono@unsa.edu.pe>
2022-06-16 00:03:25 -0500

    Se registra la app accounts en el proyecto, además que se va maquetando un layout, después el template para la vista de signup, teniendo en cuenta las indicaciones de las Diapositivas del Django3

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -6,7 +6,7 @@ def user_login(request):
     return HttpResponse("Here goes the Login View")
 
 def user_signup(request):
-    return HttpResponse("Here goes the SignUp View")
+    return render(request, "accounts/signup.html")
 
 def user_logout(request):
     return HttpResponse("Logout...")
