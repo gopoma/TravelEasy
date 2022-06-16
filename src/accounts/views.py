5db5d7a12b355cb6b88446053dc8502fef3d137c
Gustavo <gordono@unsa.edu.pe>
2022-06-16 18:01:45 -0500

    Teniendo en cuenta el video se realizó la implementación del registro de usuarios en la vister user_signup exitosamente

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def user_login(request):
@@ -7,7 +8,23 @@ def user_login(request):
 
 def user_signup(request):
     if request.method == "POST":
-        print(request.POST)
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password_confirmation = request.POST["password_confirmation"]
+
+        newUser = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        print("The user was CREATED", newUser)
+        return redirect("/")
     return render(request, "accounts/signup.html")
 
 def user_logout(request):
