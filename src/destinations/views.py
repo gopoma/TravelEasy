c13c0b0c87bec86ae640734f1f8a8e65e9fbbbde
Gustavo <gordono@unsa.edu.pe>
2022-06-16 20:42:49 -0500

    Se pasa el parámetro id_destination en el contexto para el template destination.html, previamente independizado, mostrándose exitosamente

--- src/destinations/views.py
+++ src/destinations/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import DestinoTuristico
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -8,4 +7,4 @@ def home(request):
     return render(request, "home.html", {"destinations": destinations})
 
 def show_details(request, id_destination):
-    return HttpResponse(f"Target destination's id: {id_destination}")
+    return render(request, "destinations/destination.html", {"id_destination": id_destination})
