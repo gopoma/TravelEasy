93ee6565d26cc9d37d5cfcf62beac5824d82f554
Gustavo <gordono@unsa.edu.pe>
2022-06-16 20:45:16 -0500

    Se implementa la funcionalidad de obtener un DestinoTuristico por su id, aunque falla cuando se hace referencia a un id cuyo DestinoTuristico no Existe

--- src/destinations/views.py
+++ src/destinations/views.py
@@ -7,4 +7,6 @@ def home(request):
     return render(request, "home.html", {"destinations": destinations})
 
 def show_details(request, id_destination):
+    destination = DestinoTuristico.objects.get(id=id_destination)
+    print(destination)
     return render(request, "destinations/destination.html", {"id_destination": id_destination})
